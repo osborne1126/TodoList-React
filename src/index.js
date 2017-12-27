d62ee799e1cfc168385bcfde18f91e538cbd27b2
0sborne1126 <1126340628@qq.com>
2017-12-27 18:46:53 +0800

    props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; 
 import registerServiceWorker from './registerServiceWorker';
-ReactDOM.render(
+/*ReactDOM.render(
     <h1>Hi, world!</h1>,
     document.getElementById('root2')
   );
+*/
+
+ /* function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(
+      element,
+      document.getElementById('root')
+    );
+  }
+  
+  setInterval(tick, 1000);
+  */
+
+ /* class Welcome extends React.Component { 
+    render() {
+      return <h1>Hello, Component</h1>;
+    }
+  }
+  
+  ReactDOM.render(
+    <Welcome/>,
+    document.getElementById('root')
+  )
+  */
+
+import Welcome from './Welcome'
+
+ReactDOM.render(
+ /* <Welcome/>, */
+  <Welcome name="frank"/>,
+  document.getElementById('root')
+)
+
