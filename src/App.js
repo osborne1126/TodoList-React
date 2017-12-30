8f51276d2ed9b0f32cf500207d12cbb506838075
0sborne1126 <1126340628@qq.com>
2017-12-30 16:27:06 +0800

    新增 todo

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      newTodo: 'test',
+    /*  newTodo: 'test', */
+      newTodo: '',
       todoList: [
-        {id:1, title:'第一个待办'},
-        {id:2, title:'第二个待办'},
+    /*    {id:1, title:'第一个待办'},
+        {id:2, title:'第二个待办'}, */
       ]
     }
   }
@@ -20,18 +21,21 @@ class App extends Component {
 
     let todos = this.state.todoList.map((item,index)=>{
         return ( // 为什么这里要加个括号？这是动手题3 🐸
-          <li>
+      /*    <li> */
+          <li key={index} >
             <TodoItem todo={item} />
           </li>
         )
       })
+      console.log(todos)
 
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
      {/*   <TodoInput content={this.state.newTodo} /> */}
-        <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} />
+      {/*  <TodoInput content={this.state.newTodo} onSubmit={this.addTodo} /> */}
+        <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
       </div>
         <ol>
           {todos}
@@ -39,9 +43,27 @@ class App extends Component {
       </div>
     )
   }
-  addTodo(){
-    console.log('我得添加一个 todo 了')
+ /* addTodo(){
+    console.log('我得添加一个 todo 了') */
+    addTodo(event){
+      this.state.todoList.push({
+        id: idMaker(),
+        title: event.target.value,
+        status: null,
+        deleted: false
+      })
+      this.setState({
+        newTodo: '',
+        todoList: this.state.todoList
+      })
   }
 }
 
 export default App;
+
+let id = 0
+
+function idMaker(){
+  id += 1
+  return id
+}
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -10,7 +10,8 @@ export default class TodoInput extends Component {
   }
   submit(e){
     if (e.key === 'Enter') {
-      this.props.onSubmit.call()
+    /*  this.props.onSubmit.call() */
+      this.props.onSubmit(e)
     }
   }
 }
