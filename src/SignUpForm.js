7b07badc83b35d27c331b054df7a103f525faa3e
0sborne1126 <1126340628@qq.com>
2018-01-08 23:46:20 +0800

    完善row

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -19,7 +19,7 @@ export default function (props) {
           onChange={props.onChange.bind(null, 'password')}/>
       </div>
       <div className="row actions">
-       <button type="submit">注册</button>
+       <button type="submit" id="submit">注册</button>
       </div>
     </form>
   )
